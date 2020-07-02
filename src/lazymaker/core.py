9bd3a92b414f6dad713a390cf44f032318f6bc3f
Doug V <dougmvieira@gmail.com>
2020-07-02 19:26:35 +0100

    Removed Cache class

--- src/lazymaker/core.py
+++ src/lazymaker/core.py
@@ -1,30 +1,22 @@
 import json
-from collections import namedtuple
 
 from dask.base import tokenize
 
 
-Cache = namedtuple('Cache', ('value', 'filename'))
-
-
 def check_dependencies(cache, filename, *args, **kwargs):
-    non_cache_args = tuple((arg for arg in args if not isinstance(arg, Cache)))
-    input_hash = tokenize((non_cache_args, kwargs))
-    cache_hashes = {arg.filename: cache[arg.filename][0]
-                    for arg in args if isinstance(arg, Cache)}
+    input_hash = tokenize((args, kwargs))
     try:
-        _, cached_input_hash, cached_cache_hashes = cache[filename]
-        is_updated = (input_hash == cached_input_hash
-                      ) and (cache_hashes == cached_cache_hashes)
+        _, cached_input_hash = cache[filename]
+        is_updated = input_hash == cached_input_hash
     except KeyError:
         is_updated = False
-    return is_updated, input_hash, cache_hashes
+    return is_updated, input_hash
 
 
 def update_dependencies(cache, cache_filename, output_filename, output,
-                        input_hash, cache_hashes):
+                        input_hash):
     output_hash = tokenize(output)
-    cache[output_filename] = [output_hash, input_hash, cache_hashes]
+    cache[output_filename] = [output_hash, input_hash]
     with open(cache_filename, 'w') as f:
         json.dump(cache, f, indent=4)
 
@@ -45,16 +37,14 @@ def persist_memoise(cache_filename, read, persist=None):
             def compute(*args, **kwargs):
                 return read(filename)
 
-        is_updated, input_hash, cache_hashes = check_dependencies(
+        is_updated, input_hash = check_dependencies(
             cache, filename, *args, **kwargs)
         if is_updated:
             output = read(filename)
         else:
-            args_vals = (arg.value if isinstance(arg, Cache) else arg
-                         for arg in args)
-            output = compute(*args_vals, **kwargs)
+            output = compute(*args, **kwargs)
             persist(output, filename)
             update_dependencies(cache, cache_filename, filename, output,
-                                input_hash, cache_hashes)
-        return Cache(output, filename)
+                                input_hash)
+        return output
     return closure
--- tests/test_lazymaker.py
+++ tests/test_lazymaker.py
@@ -26,22 +26,22 @@ def test():
     assert counter0 == counters[0] == 1
 
     counter0 = memo('counter0', evals_count, 0)
-    assert counter0.value == counters[0] == 2
+    assert counter0 == counters[0] == 2
 
     counter0 = memo('counter0', evals_count, 0)
-    assert counter0.value == counters[0] == 2
+    assert counter0 == counters[0] == 2
 
     counter1 = memo('counter1', evals_count, 1)
-    assert counter0.value == counters[0] == 2
-    assert counter1.value == counters[1] == 1
+    assert counter0 == counters[0] == 2
+    assert counter1 == counters[1] == 1
 
     counter1 = memo('counter1', evals_count, 1)
-    assert counter0.value == counters[0] == 2
-    assert counter1.value == counters[1] == 1
+    assert counter0 == counters[0] == 2
+    assert counter1 == counters[1] == 1
 
     sum_counters = memo('sum', int.__add__, counter0, counter1)
-    assert counter0.value == counters[0] == 2
-    assert counter1.value == counters[1] == 1
-    assert sum_counters.value == sum(counters)
+    assert counter0 == counters[0] == 2
+    assert counter1 == counters[1] == 1
+    assert sum_counters == sum(counters)
 
     os.remove(cache_filename)
